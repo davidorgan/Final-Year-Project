e8b03a8446a66203ad7693cf214b2a4477ad6443
David Organ <david.sorgan@gmail.com>
2011-01-20 22:09:55 +0000

    Added fontface

--- system/application/views/main.php
+++ system/application/views/main.php
@@ -2,9 +2,10 @@
 <html lang="en">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>960 Grid System &mdash; Demo</title>
+<title>Social Network for Clubs and Socs - 'Oh, so they have Internet on computers now!'</title>
 <link rel="stylesheet" href= "<?php echo base_url(); ?>system/application/css/reset.css" />
 <link rel="stylesheet" href="<?php echo base_url(); ?>system/application/css/text.css" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>system/application/css/font.css" type="text/css" charset="utf-8">
 <link rel="stylesheet" href="<?php echo base_url(); ?>system/application/css/960_24_col.css" />
 <!--<link rel="stylesheet" href="<?php echo base_url(); ?>system/application/css/demo.css" />-->
 
@@ -14,20 +15,28 @@
 
 </head>
 <body>
-
+	<div class="container_24">
+	<h1>
+		Social Network for Clubs and Socs
+	</h1>
+	</div>
 <div id="main_container" class="container_24">
-	<h2>
-		24 Column Grid
-	</h2>
+
 	<div id="left_panel" class="grid_8">
+		<h6 class="left-marg">
+			Left panel.
+		</h6>
 		<p>
-			left_panel
+			Quis purus vestibulum, pellentesque vehicula ac, vehicula in ipsum ac odio vehicula, eros vitae a ac scelerisque phasellus. Aenean platea est vitae vivamus mi, elit orci ligula dictum rhoncus donec taciti, enim ac quisque volutpat feugiat justo. Hendrerit hymenaeos vitae ligula mauris condimentum, amet integer ut leo id, in do vestibulum. Fusce viverra sunt mattis, vel eros et erat pellentesque luctus. Feugiat urna, vivamus dictum fusce pulvinar, pellentesque dolor fermentum leo nullam eros, elementum et, eget quis.
 		</p>
 	</div>
 	<!-- end left_panel -->
-	<div id="main_panel" class="grid_16">
+	<div id="main_panel" class="grid_15">
+		<h6 class="left-marg">
+			Main panel.
+		</h6>
 		<p>
-			main_panel
+			Lorem ipsum dolor sit amet, porttitor consequat suscipit, dolor nec, sodales ultricies massa eros leo ut amet. Sollicitudin egestas, vulputate nullam et velit laoreet vel nulla, rhoncus parturient. Urna non nulla ut. Duis porttitor suspendisse ipsum. Ante erat tristique erat dignissim vivamus. Donec cursus elit. Parturient et non fusce pharetra, posuere id justo fusce mauris vehicula, feugiat non, metus magna, mauris lacinia donec integer. Orci urna scelerisque sit morbi luctus inceptos, lacus purus dolore elit urna tortor nonummy. Quis nunc elit id maecenas. Fringilla pellentesque morbi gravida. Aliquam suspendisse convallis, aliquam vivamus, risus tristique quis leo praesent.
 		</p>
 	</div>
 	<!-- end main_panel -->
